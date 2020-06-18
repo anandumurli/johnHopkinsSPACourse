c656f5a31000b79a8b0aac45264f69b2e046173f
anandumurli <anandu.murlidharan5@gmail.com>
2020-06-18 08:36:34 +0530

    Basic UI complete, working on logic

--- module1/app.js
+++ module1/app.js
@@ -1,5 +1,19 @@
 (function(){
     angular.module('module1',[]).controller('healtyEating',function($scope){
-        $scope.name = "blahhhh"
+        $scope.name = "blahhhh";
+        $scope.funckx = function(){
+            var inputValues = $scope.name.split(',')
+            var emptyEle = inputValues.forEach(function(element){
+                element.replace(/\s{0,}/,'blank area')
+                
+//                return element != 'blank area'
+            })
+//            var cntr = 0
+//            inputValues.forEach(function(el){
+//                console.log(el.replace(/\s{0,}/,'white splace'))
+//            })
+            console.log(inputValues)
+            console.log(emptyEle)
+        };
     });
 })();
